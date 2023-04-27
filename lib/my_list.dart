8a9b780040adb1263c0cce29b41daf196aa876e6
dabuzida <dabuzida@gmail.com>
2023-04-27 16:55:52 +0900

    test

--- lib/my_list.dart
+++ lib/my_list.dart
@@ -3,13 +3,37 @@ import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
 
-class MyList extends StatelessWidget {
-  MyList({super.key});
+class MyList extends StatefulWidget {
+  const MyList({super.key});
 
-  final ValueNotifier<List<int>> list = ValueNotifier<List<int>>(<int>[]);
+  @override
+  State<MyList> createState() => _MyListState();
+}
+
+class _MyListState extends State<MyList> {
+  // final ValueNotifier<List<int>> list = ValueNotifier<List<int>>(<int>[]);
+  ValueNotifier<List<int>>? list;
+
+  @override
+  void initState() {
+    _fetch();
+    super.initState();
+  }
+
+  Future<void> _fetch() async {
+    await Future<void>.delayed(const Duration(seconds: 1));
+    list = ValueNotifier<List<int>>(<int>[]); // notifyListeners x
+    list!.value = <int>[9999]; // notifyListeners x
+    await Future<void>.delayed(const Duration(seconds: 1));
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
+    print('build');
+    if (list == null) {
+      return const Center(child: CircularProgressIndicator());
+    }
     return Center(
       child: Container(
         width: 800,
@@ -18,13 +42,15 @@ class MyList extends StatelessWidget {
         child: Row(
           children: <Widget>[
             AnimatedBuilder(
-              animation: list,
+              animation: list!,
               builder: (BuildContext context, Widget? child) {
+                print('AnimatedBuilder');
+                print(list!.hashCode);
                 return Container(
                   color: Colors.red[100],
                   width: 300,
                   height: 300,
-                  child: Center(child: Text(list.value.toString())),
+                  child: Center(child: Text(list!.value.toString())),
                 );
               },
             ),
@@ -34,8 +60,11 @@ class MyList extends StatelessWidget {
             ),
             TextButton(
               onPressed: () {
-                // list.value = <int>[1, 2, 3];
-                list.value.addAll(<int>[1, 2, 3]);
+                DateTime x = DateTime.now();
+                final int s = x.second;
+                list!.value = <int>[1, 2, 3];
+                print(list!.hashCode);
+                // list.value.addAll(<int>[1, 2, 3]);
               },
               child: Text('++++++++'),
             ),
